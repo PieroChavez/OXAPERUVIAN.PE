43f9d72852a9dba48c343b03027c0117fd0f627b
PieroChavez <artebaristachyo@gamail.com>
2023-10-26 19:33:46 -0500

    eliminacion del footer ipipipi

---
+++ resources/views/layouts/footeruno.blade.php
@@ -0,0 +1,72 @@
+<div class="container">
+    <div class="footer-cta pt-5 pb-5">
+        <div class="row">
+            <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="cta-text">
+                        <h4>Direccion</h4>
+                        <span>OXAPAMPA - PASCO - PERU</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                    <i class="fas fa-phone"></i>
+                    <div class="cta-text">
+                        <h4>Contacto</h4>
+                        <span>945848943 </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                    <i class="far fa-envelope-open"></i>
+                    <div class="cta-text">
+                        <h4>Mail </h4>
+                        <span>contacto@oxaperuvian.com</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-content pt-5 pb-5">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-logo">
+                        <a href="" class="img-fluid" alt="logo"></a>
+                    </div>
+                    <div class="footer-text">
+                        <p>Lideres en el mercado local e internacional</p>
+                    </div>
+                    <div class="footer-social-icon">
+                        <span>SIGUENOS</span>
+                        <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                        <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                        <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+               
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading">
+                        <h3>Subscribe</h3>
+                    </div>
+                    <div class="footer-text mb-25">
+                        <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                    </div>
+                    <div class="subscribe-form">
+                        <form action="#">
+                            <input type="text" placeholder="Email Address">
+                            <button><i class="fab fa-telegram-plane"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -185,6 +185,8 @@
 
 
 
+
+
 @endsection
 
 
